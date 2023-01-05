5c182b5912271557020e3863504bcda6d1c6f581
guninder <sandhujava@gmail.com>
2023-01-05 13:59:08 +1100

    start project

---
+++ EclipseIntegration/src/eclipse/Test.java
@@ -0,0 +1,13 @@
+package eclipse;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s="hello";
+		System.out.println(s);
+
+	}
+
+}
